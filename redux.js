b1a0aaa44a7c71f61bcc32ef96d06f32e4c8e1ed
tangguhriyadi <mtangguh97@gmail.com>
2022-08-03 11:07:14 +0700

    try disptaching reducer ADD_VALUE

--- redux.js
+++ redux.js
@@ -1,21 +1,34 @@
-
-
 const redux = require('redux')
 const createStore = redux.createStore
 
+// global state
 const initialState = {
-    value:'123'
+    value:1,
+    value2:2
 }
 
 //bikin reducer
-
 const rootReducer = (state = initialState, action) => {
+    console.log(action)
+    if(action.type === 'ADD_VALUE'){
+        return {
+            ...state,
+            value2: state.value2 + 1
+        }
+    }
     return state;
 
 }
 
-//store
+//bikin store
 const store = createStore(rootReducer);
+console.log(store.getState())
+
+//dispatching
+store.dispatch({
+    type:'ADD_VALUE'
+})
+
 console.log(store.getState())
 
 
