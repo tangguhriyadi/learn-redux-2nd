75cea38fe4d8e175ce2226536c2a188754e02e87
tangguhriyadi <mtangguh97@gmail.com>
2022-08-03 10:51:56 +0700

    create 1st store and initial state

--- redux.js
+++ redux.js
@@ -1,3 +1,21 @@
 
 
+const redux = require('redux')
+const createStore = redux.createStore
+
+const initialState = {
+    value:'123'
+}
+
+//bikin reducer
+
+const rootReducer = (state = initialState, action) => {
+    return state;
+
+}
+
+//store
+const store = createStore(rootReducer);
+console.log(store.getState())
+
 
